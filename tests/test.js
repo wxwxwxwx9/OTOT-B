b2d4fa8b02b3171d2b4c531d428a052bc3f90af1
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2022-09-03 22:41:39 +0800

    add mocha steps

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-import chai, { assert } from 'chai';
+import chai, { assert, expect } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../index.js';
 import contactModel from '../contactModel.js';
@@ -15,58 +15,56 @@ const testContact = {
 };
 
 describe("Contacts", () => {
-	describe("GET /api/contacts", () => {
-		it("should get all contacts", async () => {
-			chai.request(app)
+	describe("API test", () => {
+		step("GET /api/contacts", async () => {
+			const res = await chai
+				.request(app)
 				.get('/api/contacts')
-				.end((err, res) => {
-					res.should.have.status(200);
-					res.body.should.be.a('object');
-				});
+
+			res.should.have.status(200);
+			res.body.should.be.a('object');
 		});
-	});
-	describe("POST /api/contacts", () => {
-		it("should add a new contact", async () => {
-			chai.request(app)
+		step("POST /api/contacts", async () => {
+			const res = await chai
+				.request(app)
 				.post('/api/contacts')
-				.send(testContact)
-				.end((err, res) => {
-					res.should.have.status(200);
-					res.body.should.be.a('object');
-				});
+				.send(testContact);
+
+			res.should.have.status(200);
+			res.body.should.be.a('object');
 		});
-	});
-	describe("PUT /api/contacts/{id}", () => {
-		it("should update an existing contact", async () => {
+		step("PUT /api/contacts/{id}", async () => {
 			const contacts = await contactModel
 				.find(testContact)
 				.exec();
 
-			chai.request(app)
+			const res = await chai
+				.request(app)
 				.put(`/api/contacts/${contacts[0]._id}`)
-				.send(testContact)
-				.end((err, res) => {
-					res.should.have.status(200);
-					res.body.should.be.a('object');
-				});
+				.send(testContact);
+
+			res.should.have.status(200);
+			res.body.should.be.a('object');
 		});
-	});
-	describe("DELETE /api/contacts/{id}", () => {
-		it("should delete an existing contact", async () => {
+		step("DELETE /api/contacts/{id}", async () => {
 			const contacts = await contactModel
 				.find(testContact)
 				.exec();
+
 			assert(contacts.length > 0, 'must have at least 1 contact from previous POST test');
 
-			contacts.forEach((contact) => {
-				chai.request(app)
-					.delete(`/api/contacts/${contact._id}`)
-					.send(testContact)
-					.end((err, res) => {
-						res.should.have.status(200);
-						res.body.should.be.a('object');
-					});
-			});
+			const outcome = await Promise.all(
+				contacts.map(async (contact) => {
+					const res = await chai.request(app)
+						.delete(`/api/contacts/${contact._id}`)
+						.send(testContact);
+
+					return res.status === 200 && typeof res.body === 'object';
+				})
+			);
+			if (outcome.includes(false)) {
+				assert(false, 'failed to delete one of the contacts');
+			}
 		});
 	});
 });
