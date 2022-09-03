eac0c3e05a1b64e5590679f99b70f3669cd9791b
wxwxwxwx9 <37920115+wxwxwxwx9@users.noreply.github.com>
2022-09-04 00:44:49 +0800

    Update db.js

--- db.js
+++ db.js
@@ -6,10 +6,10 @@ const MONGODB_URI = 'mongodb://localhost/app';
 const open = async () => {
 	if (process.env.NODE_ENV === 'test') {
 		const mockgoose = new Mockgoose(mongoose);
+		throw new Error("before");
 		await mockgoose.prepareStorage();
 	}
 
-	throw new Error("before");
 	mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 	mongoose.connection.on('connected', () => {
 		console.log('mongoose now connected')
